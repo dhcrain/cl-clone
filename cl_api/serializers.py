6e374a688e96d4822510ebbd83f02bcb3fa38ebf
dhcrain <dhcrain@gmail.com>
2016-06-30 16:30:54 -0400

    made API endpoints for listing, cat, sub cat, working on listings by cat

--- cl_api/serializers.py
+++ cl_api/serializers.py
@@ -1,4 +1,4 @@
-from cl_app.models import Listing
+from cl_app.models import Listing, ListingType
 from rest_framework import serializers
 
 class ListingSerializer(serializers.ModelSerializer):
@@ -6,3 +6,10 @@ class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
         fields = ['id', 'user', 'listing_city', 'category', 'title', 'price', 'description', 'photo', 'created']
+
+
+class ListingTypeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ListingType
+        fields = ['id', 'name', 'parent']
--- cl_api/views.py
+++ cl_api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
-from cl_app.models import Listing
-from cl_api.serializers import ListingSerializer
+from cl_app.models import Listing, ListingType
+from cl_api.serializers import ListingSerializer, ListingTypeSerializer
 # Create your views here.
 
 
@@ -9,6 +9,52 @@ class ListingListCreateAPIView(generics.ListCreateAPIView):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
 
+
 class ListingRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
+
+
+class CategoryListAPIView(generics.ListAPIView):
+    serializer_class = ListingTypeSerializer
+
+    def get_queryset(self):
+        return ListingType.objects.filter(parent=None)
+
+class CategoryRetriveAPIView(generics.RetrieveAPIView):
+    serializer_class = ListingTypeSerializer
+
+    def get_queryset(self):
+        return ListingType.objects.filter(parent=None)
+
+
+class SubCategoryListAPIView(generics.ListAPIView):
+    serializer_class = ListingTypeSerializer
+
+    def get_queryset(self):
+        return ListingType.objects.exclude(parent=None)
+
+
+class SubCategoryRetriveAPIView(generics.RetrieveAPIView):
+    serializer_class = ListingTypeSerializer
+
+    def get_queryset(self):
+        return ListingType.objects.exclude(parent=None)
+
+
+class CategoryListingListAPIView(generics.ListAPIView):
+    serializer_class = ListingSerializer
+
+    def get_queryset(self, **kwargs):
+        cat_pk = self.kwargs.get('pk')
+        return Listing.objects.filter(category=cat_pk)
+
+class CategoryListingRetriveAPIView(generics.RetrieveAPIView):
+    serializer_class = ListingSerializer
+
+    def get_queryset(self, **kwargs):
+        cat_pk = self.kwargs.get('pk')
+        listing_pk = self.kwargs.get('listing_pk')
+        print(cat_pk)
+        print(listing_pk)
+        return Listing.objects.get(pk=listing_pk)
