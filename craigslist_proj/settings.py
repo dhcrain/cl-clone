5e55a22ef9122f968ff99d1d041fa9e3dbbef4cb
dhcrain <dhcrain@gmail.com>
2016-06-30 13:42:58 -0400

    installed Django REST Framework

--- craigslist_proj/settings.py
+++ craigslist_proj/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cl_app'
+    'cl_app',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE_CLASSES = [
